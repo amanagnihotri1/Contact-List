ee84deb0b86d31aafd7d3efd0f92fec161dbd3bf
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2021-08-28 13:17:36 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://Aman:Chaikidukan1@cluster0.kwlnx.mongodb.net/wikiDB?retryWrites=true&w=majority",{ useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect("mongodb+srv://Aman:Chaikidukan1@cluster0.kwlnx.mongodb.net/ContactList?retryWrites=true&w=majority",{ useUnifiedTopology:true,useNewUrlParser:true});
 const db=mongoose.connection;
 db.on('error', console.error.bind(console,'connection error connecting to db'));
 db.once('open', function () {
